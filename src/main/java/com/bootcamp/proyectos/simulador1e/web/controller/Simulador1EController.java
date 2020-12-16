c20eb29d7c7ab8d99e113010fe89e509e7a1873e
Joseph Toulier Pighi <jtoulier@gmail.com>
2020-12-15 19:04:02 -0500

    Manejando errores HTTP

--- src/main/java/com/bootcamp/proyectos/simulador1e/web/controller/Simulador1EController.java
+++ src/main/java/com/bootcamp/proyectos/simulador1e/web/controller/Simulador1EController.java
@@ -4,7 +4,9 @@ import com.bootcamp.proyectos.simulador1e.domain.dto.ResultadoSimulacionRequest;
 import com.bootcamp.proyectos.simulador1e.domain.dto.ResultadoSimulacionResponse;
 import com.bootcamp.proyectos.simulador1e.domain.services.Simulador1EService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +23,16 @@ public class Simulador1EController {
     @PostMapping(value = "/",
                     consumes = MediaType.APPLICATION_JSON_VALUE,
                     produces = MediaType.APPLICATION_JSON_VALUE)
-    ResultadoSimulacionResponse obtenerResultadoSimulacion(@Valid @RequestBody ResultadoSimulacionRequest resultadoSimulacionRequest) {
-        return simulador1EService.obtenerResultadoSimulacion(resultadoSimulacionRequest);
+    ResponseEntity<ResultadoSimulacionResponse> obtenerResultadoSimulacion(@Valid @RequestBody ResultadoSimulacionRequest resultadoSimulacionRequest) {
+        ResultadoSimulacionResponse resultadoSimulacionResponse =simulador1EService.obtenerResultadoSimulacion(resultadoSimulacionRequest);
+        String estado = resultadoSimulacionResponse.getEstado();
+
+        if (estado.equals("EXITOSO")) {
+            return new ResponseEntity<>(resultadoSimulacionResponse, HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(resultadoSimulacionResponse, HttpStatus.BAD_REQUEST);
+        }
+
     }
 }
